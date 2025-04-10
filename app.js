6a17e1b317b14c786ac92b4f816c3346e060f325
Mary Dawn Malida <157961578+PseuDawncode@users.noreply.github.com>
2025-04-10 11:54:56 +0200

    Added class  to represents a book

---
+++ app.js
@@ -0,0 +1,35 @@
+// Book Class: Represents a book
+class Book{
+    constructor(titile, author, bookid){
+        this.title = title;
+        this.author = author;
+        this.bookid = bookid;
+
+    }
+}
+
+// UI Class: Handle UI Tasks
+class UI {
+    static displyaBooks(){
+        const StoredBooks = [
+            {
+                title: 'Book One',
+                author: 'David W.',
+                bookid: '0230027'
+            },
+            {
+                title: 'Book Two',
+                author: 'Juno The Cat',
+                bookid: '0230029'
+            }
+        ];
+    }
+}
+
+// Store Class: Handles Storage
+
+// Event: Display books
+
+// Event: Add a book
+
+// Event: Remove a book
